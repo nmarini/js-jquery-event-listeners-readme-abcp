d5b907245cab256943f327e0d558ddefb364c811
Nicolas Marini <nmarini44@gmail.com>
2018-03-15 23:51:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,26 @@
 //define functions here
 
+function getIt() {
+  $(`p`).on("click", function() {alert("Hey!")})
+}
+
+function frameIt() {
+  $(`img`).on("load", function() {
+    $(`img`).addClass("tasty")
+  })
+}
+
+function submitIt() {
+  $(`form`).on("submit", function() {
+    alert(`Your form is going to be submitted now.`)
+  })
+}
+
+function pressIt() {
+  $(`input`).on(`keydown`, function(key) {
+    if(key.which == )
+  })
+}
 $(document).ready(function(){
 
 // call functions here
